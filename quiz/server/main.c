00fb2a485df82cc11e5cd3d07c46e2afc327e765
Michael Ruf <Ruf_Michael@gmx.de>
2017-05-05 23:17:59 +0200

    Add another exit hook

--- quiz/server/main.c
+++ quiz/server/main.c
@@ -77,6 +77,7 @@ int main(int argc, char **argv) {
     signal(SIGABRT, shutdownServer);
     signal(SIGINT, shutdownServer);
     signal(SIGTERM, shutdownServer);
+    atexit(shutdownServer);
 
     // Initialize and parse arguments for configuration
     CONFIGURATION config = initConfiguration();
