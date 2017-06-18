5d92db792d6b28ffe05ff2b67b140b64e353e2cb
Artur Eichler <ae-161115@hs-weingarten.de>
2017-06-18 19:53:09 +0200

    testing not relevant

--- quiz/server/user.c
+++ quiz/server/user.c
@@ -279,14 +279,14 @@ int isGameLeader(int id) {
 }
 
 //Calc score
-unsigned long scoreForTimeLeft(unsigned long timeout,unsigned long timeLeft) {
+unsigned long scoreForTimeLeft(long timeout, long timeLeft) {
     unsigned long score = (timeLeft * 1000UL)/timeout; /* auf 10er-Stellen runden */
     score = ((score+5UL)/10UL)*10UL;
     return score;
 }
 
 //Calc score and set for the user given, question timeout, needed time to answer, and clientSocket
-void calcScoreForUserByID(unsigned long timeout, unsigned long neededtime, int id) {
+void calcScoreForUserByID(long timeout, long neededtime, int id) {
 
     int scoreForCurrentQuestion = scoreForTimeLeft(timeout,timeout-neededtime);
 
